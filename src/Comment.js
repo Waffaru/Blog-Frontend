f1d941717c9afd927832924a2b97b3b4ae1dc819
Gonzalo Ortiz <gonzalo.ortizbianco@cs.tamk.fi>
2018-04-05 00:38:26 +0300

    Add Comment.js
    Comment.js is the component that handles all the comments for the post component it's attached to.

---
+++ src/Comment.js
@@ -0,0 +1,72 @@
+
+import React, { Component } from 'react';
+
+class Comment extends Component {
+    constructor(props) {
+      super(props);
+      this.fetchComments = this.fetchComments.bind(this);
+      this.state = {comments: []};
+    }
+
+    fetchComments() {
+        let url = 'http://localhost:8080/comment'
+
+        fetch(url).then((response) => response.json()).then((commentList) => {
+                            this.setState({comments: commentList});
+            console.log(this.state.comments);
+        })
+    }
+
+    componentDidMount() {
+        this.fetchComments();
+    }
+  
+    render() {
+        let _this = this;
+
+        function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}            
+        function deletePost(id, e) {
+            //TODO: Fixaa UI update
+            e.preventDefault();
+            console.log('Click');
+            var url = `http://localhost:8080/blogpost/${id}`
+            console.log(url)
+            fetch(url, {
+            method: 'DELETE'
+            }).then(response => response.json()).then();
+            //koodi jossa se menee kantaan ja käskee poistamaan tossa urlissa olevaa dataa
+
+            sleep(100).then(() => {
+                fetch(`http://localhost:8080/blogpost`).then((response) => response.json()).then((blogList) => {
+                            _this.setState({posts: blogList});
+                console.log(_this.state.posts);
+                })            
+            })
+        }
+
+        function dislikePost(id, e) {
+            e.preventDefault();
+            console.log('Dislike');
+            console.log(id)
+            var url = `http://localhost:8080/comment/${id}/dislike`
+            fetch(url);     
+        }          
+
+
+
+        let commentList = this.state.comments;
+        var commentArr = []
+        for(let comment of commentList) {
+            commentArr.push(<div>
+            <p>{comment.username}</p>
+            <p> {comment.date} @ {comment.dislikes}</p>
+            <p>{comment.body}</p>
+            <button id={`edit-${comment.id}`}>Edit</button>
+            <button id={`dlt-${comment.id}`}  onClick={(e) => deletePost(comment.id, e)}>Delete</button>
+            <button id={`dislike-${comment.id}`} onClick={(e) => dislikePost(comment.id, e)}> Dislike </button> </div>)
+        }
+        return <div>{commentArr}</div>
+    }
+  }
+
+  export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Login from './Login';
+import Comment from './Comment';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 //TODO: Fix Login
 ReactDOM.render(<Login />, document.getElementById('login'));
 ReactDOM.render(<App />, document.getElementById('root'));
-/*ReactDOM.render(<Field />, document.getElementById('field'));
-ReactDOM.render(<BlogPost />, document.getElementById('blogPost'));*/
+//ReactDOM.render(<Comment />, document.getElementById('root'));
 registerServiceWorker();
